953f64d7118c5c5f344e31292c1412d0b1856839
Ricky Han <hzgdiyer@163.com>
2018-10-22 23:48:21 +0800

    complete function: reverse_sentence

--- string_reverse.c
+++ string_reverse.c
@@ -1,13 +1,16 @@
+#include "string.h"
+#include "stdlib.h"
+#include <string.h>
 /*
-input string: “Hi buddy, welcome to Alibaba.”，
-output string:“Alibaba. to welcome buddy, Hi”。
+input string: “Alibaba.”，
+output string:“ababilA”。
 */
 
 char *reverse_string(char *beg, char *end)
 {
 	char *r = beg;
 	char tmp;
-	if(NULL==beg || NULL=end)
+	if(NULL==beg || NULL==end)
 		return NULL;
 
 	while(beg < end)
@@ -23,6 +26,10 @@ char *reverse_string(char *beg, char *end)
 	return r;
 }
 
+/*
+input sentence: “Hi buddy, welcome to Alibaba.”，
+output sentence:“Alibaba. to welcome buddy, Hi”。
+*/
 char *reverse_sentence(char *in)
 {
 	char *beg=NULL, *end=NULL;
@@ -33,22 +40,22 @@ char *reverse_sentence(char *in)
 	beg = in;
 	end = beg;
 
-	while(' ' != *end++);
-	if('\n' != *end)
+	while(end < in+strlen(in)-1)
 	{
+		while(' ' != *end && '\0' != *end)
+			end++;
+
 		end--;
 		reverse_string(beg, end);
-		
-		beg = end;
-		beg++;
-		end = beg;
-	}
-	else
-	{
+			
+		end++;
+		while(' '== *end)
+			end++;
 
+		beg = end;	
 	}
 
-	while
-	
-	
+	reverse_string(in, in+strlen(in)-1);
+
+	return in;
 }
